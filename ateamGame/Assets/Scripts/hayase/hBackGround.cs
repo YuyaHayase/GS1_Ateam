d73bac04e96d5782e27a25ab7c3ac75078c64333
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2018-03-01 15:15:19 +0900

    びちょ

--- ateamGame/Assets/Scripts/hayase/hBackGround.cs
+++ ateamGame/Assets/Scripts/hayase/hBackGround.cs
@@ -15,15 +15,9 @@ public class hBackGround : MonoBehaviour {
     [SerializeField, Header("回転速度")]
     float RotateSpeed = 2.0f;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-        if (hKeyConfig.GetKey("Zone")) transform.Rotate(new Vector3(0, 0, 1), (int)dir * RotateSpeed * Time.deltaTime / 6f);
+        if (hKeyConfig.GetKey("Zone")) transform.Rotate(new Vector3(0, 0, 1), (int)dir * RotateSpeed * Time.deltaTime / 8f);
         else transform.Rotate(new Vector3(0, 0, 1), (int)dir * RotateSpeed * Time.deltaTime);
-        transform.Rotate(new Vector3(0, 0, 1), (int)dir * RotateSpeed * Time.deltaTime);
     }
 }
