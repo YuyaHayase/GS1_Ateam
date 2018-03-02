4f5a0c6eef7b16177b80c58c148cc4909c809a87
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2018-03-02 15:09:35 +0900

    羊

---
+++ ateamGame/Assets/Scripts/hayase/hTutorialCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hTutorialCtrl : MonoBehaviour {
+
+    float delta = 0;
+
+    [SerializeField, Header("BAr")]
+    GameObject Bar;
+
+	// Use this for initialization
+	void Start () {
+        if (Bar == null) Bar = GameObject.Find("Bar");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hKeyConfig.GetKey("Submit")) delta += Time.deltaTime;
+        if (hKeyConfig.GetKeyUp("Submit")) delta = 0;
+
+        Bar.transform.localScale = new Vector3(delta, 1, 1);
+        if (delta > 5) SceneManager.LoadScene("Main");
+	}
+}
